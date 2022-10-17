0db8b86ab99e43f9c3b8faff9d66d3a8f7b231a2
Roger-Franco <rogerfrancogalt@gmail.com>
2022-10-17 18:09:25 -0400

    signup

--- README-install.md
+++ README-install.md
@@ -1,4 +1,5 @@
 Npm init
 npm install express firebase-admin nodemon 
 
+https://www.youtube.com/watch?v=F6LEKdQPSiM&list=PLSuzwxF6LC4AGZN8SrPbRkN3MrLWubOGW&ab_channel=CodewithKavit
 arquivo server e depois no firebase: Authentication e crio a autenticação pelo email e senha.
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express();
 
+const admin = require("firebase-admin")
+const credentials = require("./serviceAccountKey.json")
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+})
+
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
 // set post and listen for our requests
+
+app.post('/signup', async (req, res) => {
+  const userResponse = await admin.auth().createUser({
+    email: req.body.email,
+    password: req.body.password,
+    emailVerified: false,
+    disabled: false
+  })
+  res.json(userResponse)
+})
 
 const PORT = process.env.PORT || 8080
 
